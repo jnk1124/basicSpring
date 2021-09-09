a43da23681949e9b2474348f05e69603ef271280
jnk1124 <jnk1124@naver.com>
2021-09-10 00:03:14 +0900

    Initial commit

---
+++ src/test/java/hello/core/member/MemberServiceImplTest.java
@@ -0,0 +1,31 @@
+package hello.core.member;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class MemberServiceImplTest {
+
+    MemberService memberService ;
+
+    @BeforeEach
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
+
+    @Test
+    void join(){
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        assertThat(member).isEqualTo(findMember);
+    }
+
+
+}
